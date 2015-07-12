958e63244e2785252c7cc4b74501b22fe8004e57
Stijn Haulotte <stijnhau@gmail.com>
2015-07-12 18:53:07 +0200

    Added route

--- config/module.config.php
+++ config/module.config.php
@@ -5,4 +5,19 @@ return [
             'UserRegistration' => __DIR__ . '/../view',
         ],
     ],
+    'router' => array(
+        'routes' => array(
+            'zfcuser/register' => array(
+                'type' => 'Literal',
+                'priority' => 1000,
+                'options' => array(
+                    'route' => '/user/register',
+                    'defaults' => array(
+                        'controller' => 'register',
+                        'action'     => 'index',
+                    ),
+                ),
+            ),
+        ),
+    ),
 ];
